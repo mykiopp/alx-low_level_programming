1431f62015b78fd188870f40d31f52e226d8b855
Mikiyas Begashaw <mikiyasbegashaw44@gmail.com>
2023-10-31 12:00:12 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector of the pointers.
+ * Return: does not return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for and copies the contents to another file.
+ * @argc: number of arguments of the pointers.
+ * @argv: arguments vector of the pointers.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from;
+	int file_to;
+	int err_close;
+	ssize_t l, m;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	l = 1024;
+	while (l == 1024)
+	{
+		l = read(file_from, buf, 1024);
+		if (l == -1)
+			error_file(-1, 0, argv);
+		m = write(file_to, buf, l);
+		if (m == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
